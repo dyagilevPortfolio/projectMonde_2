f0cf252fc00138576d60e4fa9ad3320505f5ae6f
Dmitriy <dmitriy.degilev@gmail.com>
2019-06-28 15:51:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+    });
+
+	$(".arrow-down").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 900);
+    });
+    
+});
